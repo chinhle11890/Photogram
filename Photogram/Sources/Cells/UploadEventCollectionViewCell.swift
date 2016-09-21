5751ce9a94a343f6020877ee08a9ea61d99a75ad
Bao Nguyen <baonguyena1@gmail.com>
2016-09-21 12:28:39 +0700

    - add screen Event photo add

---
+++ Photogram/Sources/Cells/UploadEventCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  UploadEventCollectionViewCell.swift
+//  Photogram
+//
+//  Created by Bao Nguyen on 9/21/16.
+//  Copyright © 2016 Chinh Le. All rights reserved.
+//
+
+import UIKit
+
+class UploadEventCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var chooseImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Photogram/Sources/Controllers/UploadEventViewController.swift
@@ -0,0 +1,55 @@
+//
+//  UploadEventViewController.swift
+//  Photogram
+//
+//  Created by Bao Nguyen on 9/21/16.
+//  Copyright © 2016 Chinh Le. All rights reserved.
+//
+
+import UIKit
+
+class UploadEventViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    private let numberOfColumn: CGFloat = 3
+    private let photoCell = "photoCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoCollectionView.contentInset = UIEdgeInsetsMake(8, 8, 8, 8)
+        photoCollectionView.showsVerticalScrollIndicator = false
+        photoCollectionView.showsHorizontalScrollIndicator = false
+    }
+    
+    @IBAction func didClickBackButton(_ sender: AnyObject) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func didClickDoneBUtton(_ sender: AnyObject) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: photoCell, for: indexPath) as! UploadEventCollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (photoCollectionView.frame.width - 16 - 8*3)/numberOfColumn
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
